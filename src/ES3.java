3ea40e5ccee5ac7939f8d6b76cf0fd28688f8017
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-22 15:17:22 +0200

    Adds areaRettangolo ES3

--- src/ES3.java
+++ src/ES3.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+import static java.lang.Double.parseDouble;
+
 public class ES3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci la misura dei due lati del rettangolo per favore");
+        double primoLato = parseDouble(scanner.nextLine());
+        double secondoLato = parseDouble(scanner.nextLine());
+        System.out.println("Il perimetro del rettangolo è: " + perimetroRettangolo(primoLato, secondoLato));
+    }
 
+    public static double perimetroRettangolo(double primoLato, double secondoLato) {
+        return primoLato * secondoLato;
     }
 }
