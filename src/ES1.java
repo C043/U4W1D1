4fdcc955fe3149b9a662a275c5106cdf972568d7
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-22 13:41:34 +0200

    Adds concatena method

--- src/ES1.java
+++ src/ES1.java
@@ -2,9 +2,15 @@ public class ES1 {
     public static void main(String[] args) {
         int result = moltiplica(2, 3);
         System.out.println("Il risultato della moltiplicazione è " + result);
+        String concatenazione = concatena("count", 4);
+        System.out.println(concatenazione);
     }
 
     public static int moltiplica(int num1, int num2) {
         return num1 * num2;
+    }
+
+    public static String concatena(String stringa, int number) {
+        return (stringa + number);
     }
 }
