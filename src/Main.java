cc5da4cbada9e7c799b4622ce2a1cfb4a9cc7cae
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-22 15:04:00 +0200

    Adds main method to ES2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci tre stringhe per favore:");
+        String primaStringa = scanner.nextLine();
+        String secondaStringa = scanner.nextLine();
+        String terzaStringa = scanner.nextLine();
+        System.out.println(primaStringa + " " + secondaStringa + " " + terzaStringa);
+        System.out.println(terzaStringa + " " + secondaStringa + " " + primaStringa);
     }
 }
