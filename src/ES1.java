c2615b4b965a4c7369fb0cf5d370e29212606c2c
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-22 13:30:44 +0200

    Adds moltiplica method to ES1

--- src/ES1.java
+++ src/ES1.java
@@ -1,5 +1,10 @@
 public class ES1 {
     public static void main(String[] args) {
+        int result = moltiplica(2, 3);
+        System.out.println("Il risultato della moltiplicazione è " + result);
+    }
 
+    public static int moltiplica(int num1, int num2) {
+        return num1 * num2;
     }
 }
