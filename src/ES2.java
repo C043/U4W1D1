54a59733675b2905c73d99562ef651aee9f5b48a
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-22 15:05:36 +0200

    Fixed ES2

--- src/ES2.java
+++ src/ES2.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ES2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci tre stringhe per favore:");
+        String primaStringa = scanner.nextLine();
+        String secondaStringa = scanner.nextLine();
+        String terzaStringa = scanner.nextLine();
+        System.out.println(primaStringa + " " + secondaStringa + " " + terzaStringa);
+        System.out.println(terzaStringa + " " + secondaStringa + " " + primaStringa);
     }
 }
